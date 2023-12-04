677ed5ffe08a306be499ec881ea3688029c4ec7f
GenoKuno <erd3lu@gmail.com>
2023-12-04 19:14:02 +0800

    e

--- JS.js
+++ JS.js
@@ -369,11 +369,21 @@
 
   function toggleDarkMode() {
       var body = document.body;
+      var header = document.querySelector('header');
+      var footer = document.querySelector('footer');
       if (body.classList.contains('light-mode')) {
           body.classList.remove('light-mode');
           body.classList.add('dark-mode');
+          header.classList.remove('lightmode');
+          header.classList.add('darkmode');
+          footer.classList.remove('lightmode');
+          footer.classList.add('darkmode');
       } else {
           body.classList.remove('dark-mode');
           body.classList.add('light-mode');
+          header.classList.remove('darkmode');
+          header.classList.add('lightmode');
+          footer.classList.remove('darkmode');
+          footer.classList.add('lightmode');
       }
   }
