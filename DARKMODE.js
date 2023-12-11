655cb27107b7407fb66bd85dddda961e32416691
GenoKuno <erd3lu@gmail.com>
2023-12-11 22:20:06 +0800

    o

--- DARKMODE.js
+++ DARKMODE.js
@@ -3,22 +3,45 @@
       var header = document.querySelector('header');
       var footer = document.querySelector('footer');
       var darkmode = document.getElementById('toggle-dark-button')
+      var left = document.getElementById('left');
+      var right = document.getElementById('right');
+      var side = document.getElementById('side');
 
-      if (!body.classList.contains('dark-mode')) {
+      if (body.classList.contains('light-mode')) {
           body.classList.remove('light-mode');
           body.classList.add('dark-mode');
-          header.classList.remove('lightmode');
-          header.classList.add('darkmode');
-          footer.classList.remove('lightmode');
-          footer.classList.add('darkmode');
-          darkmode.textContent = "DAY"
+          header.classList.remove('light-header-footer');
+          header.classList.add('dark-header-footer');
+          header.classList.remove('light-border');
+          header.classList.add('dark-border');
+          footer.classList.remove('light-border');
+          footer.classList.add('dark-border');
+          footer.classList.remove('light-header-footer');
+          footer.classList.add('dark-header-footer');
+          left.classList.remove('light-text');
+          left.classList.add('dark-text');
+          right.classList.remove('light-text');
+          right.classList.add('dark-text');
+          side.classList.add('dark-border');
+          side.classList.remove('light-border');
+          darkmode.textContent = "LIGHT"
       } else {
           body.classList.remove('dark-mode');
           body.classList.add('light-mode');
-          header.classList.remove('darkmode');
-          header.classList.add('lightmode');
-          footer.classList.remove('darkmode');
-          footer.classList.add('lightmode');
-          darkmode.textContent = "NIGHT"
+          header.classList.remove('dark-header-footer');
+          header.classList.add('light-header-footer');
+          header.classList.add('light-border');
+          header.classList.remove('dark-border');
+          footer.classList.add('light-border');
+          footer.classList.remove('dark-border');
+          footer.classList.remove('dark-header-footer');
+          footer.classList.add('light-header-footer');
+          left.classList.add('light-text');
+          left.classList.remove('dark-text');
+          right.classList.add('light-text');
+          right.classList.remove('dark-text');
+          side.classList.add('light-border');
+          side.classList.remove('dark-border');
+          darkmode.textContent = "DARK"
         }
   }
