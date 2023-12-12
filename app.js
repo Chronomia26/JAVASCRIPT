a9dd6bfc2547cb2e74e30571788a98e880b8f8a7
GenoKuno <erd3lu@gmail.com>
2023-12-12 17:54:22 +0800

    u

--- app.js
+++ app.js
@@ -3,18 +3,16 @@ function compile() {
   var css = document.getElementById("css");
   var js = document.getElementById("js");
   var code = document.getElementById("code").contentWindow.document;
-
+  var transferredText = sessionStorage.getItem('textToTransfer');
+  var iframe = document.getElementById('code')
+  document.getElementById('html').value = transferredText;
+  var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+  iframeDocument.open();
+  iframeDocument.writeln(transferredText);
+  iframeDocument.close();
   document.body.onkeyup = function() {
     code.open();
-    code.writeln(
-      html.value +
-      "<style>" +
-      css.value +
-      "</style>" +
-      "<script>" +
-      js.value +
-      "</script>"
-    );
+    code.writeln(html.value);
     code.close();
   };
 }
